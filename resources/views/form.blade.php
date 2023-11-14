3caa2e5ecf8d517f29d7a2b24cee3846fd4eeafc
auizadi <auizaldi.21026@mhs.unesa.ac.id>
2023-11-14 08:50:37 +0700

    Mengerjakan tampilan

---
+++ resources/views/form.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/form-style.css">
+    <title>Form</title>
+</head>
+<body>
+    <div class="grid-container">
+        <div class="grid-item">
+            <p>Tulis Laporan Disini</p>
+            <form action="">
+                
+                <input type="text" placeholder="Masukkan Judul Laporan">
+                <br>
+                <br>
+                <textarea name="" id="" cols="30" rows="10" placeholder="Masukkan Isi Laporan"></textarea>
+                <br>
+                <input type="date" name="" id="">
+                <br><br>
+                <textarea name="" id="" cols="30" rows="10" placeholder="Lokasi Kampus"></textarea>
+                <br>
+                
+                
+                <select name="" id="" placeholder="asdasd" >
+                    <option value="" >Pilih Kategori Kejadian</option>
+                    <option value="">Sarana Prasarana</option>
+                    <option value="">Sarana Prasarana</option>
+                </select>
+                <br>
+                <input type="file" name="" id="file">
+                <br>
+                <br>
+               
+                <button>KIRIM</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
